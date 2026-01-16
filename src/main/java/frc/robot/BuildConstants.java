6ffefb2234ba467771fd88779a5aaaf33a23e86b
Rui Rebich Hespanha <Rui.hespanha@gmail.com>
2026-01-16 14:58:33 -0800

    created new bratch and added git ignore

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 34;
-  public static final String GIT_SHA = "6e05c5e3e54b61838a30c4b1a4b645f890741140";
-  public static final String GIT_DATE = "2026-01-16 02:30:24 GMT";
+  public static final int GIT_REVISION = 36;
+  public static final String GIT_SHA = "bf4611eb2b6dc80dc835daef12d0bfde3dd0a785";
+  public static final String GIT_DATE = "2026-01-16 22:46:34 GMT";
   public static final String GIT_BRANCH = "Main";
-  public static final String BUILD_DATE = "2026-01-16 03:08:58 GMT";
-  public static final long BUILD_UNIX_TIME = 1768532938508L;
+  public static final String BUILD_DATE = "2026-01-16 22:48:51 GMT";
+  public static final long BUILD_UNIX_TIME = 1768603731191L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
