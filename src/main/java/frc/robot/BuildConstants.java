a4eac32cdad81478c5bbb9a3f4fb19123f64e907
MichaelStevenShinn <michaelstevenshinn@gmail.com>
2026-01-23 21:05:07 -0800

    Fixed swerve, onRamp working

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 43;
-  public static final String GIT_SHA = "772d471d42393171ebe6c1f412109acd45f0849b";
-  public static final String GIT_DATE = "2026-01-20 03:33:27 GMT";
-  public static final String GIT_BRANCH = "coolSwerve";
-  public static final String BUILD_DATE = "2026-01-20 04:01:26 GMT";
-  public static final long BUILD_UNIX_TIME = 1768881686040L;
+  public static final int GIT_REVISION = 45;
+  public static final String GIT_SHA = "4ab886c083813606c9f0b77950da3a6af9fb82d4";
+  public static final String GIT_DATE = "2026-01-22 02:09:03 GMT";
+  public static final String GIT_BRANCH = "2026-autos";
+  public static final String BUILD_DATE = "2026-01-22 04:20:32 GMT";
+  public static final long BUILD_UNIX_TIME = 1769055632438L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
