d748a429cddf64561203f8e229fa5fc128255b02
MichaelStevenShinn <michaelstevenshinn@gmail.com>
2026-01-28 20:37:08 -0800

    Outpost auto

---
+++ src/main/java/frc/robot/commands/auto/StateChange/SetStateManual.java
@@ -0,0 +1,8 @@
+package frc.robot.commands.auto.StateChange;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+public class SetStateManual extends InstantCommand{
+    public SetStateManual () {
+    }
+}
---
+++ src/main/java/frc/robot/commands/auto/StateChange/SetStateSafe.java
@@ -0,0 +1,8 @@
+package frc.robot.commands.auto.StateChange;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+public class SetStateSafe extends InstantCommand{
+    public SetStateSafe () {
+    }
+}
