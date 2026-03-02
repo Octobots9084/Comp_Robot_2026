a6c145dde37289d92c6d38f95bbed51e285f870f
Santiago7611 <bahlam935@gmail.com>
2026-03-02 11:16:53 -0800

    Auto Troubleshooting

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 176;
-  public static final String GIT_SHA = "2e8b6a4f49a5ea02d74e6bc9bba976562f939fd5";
-  public static final String GIT_DATE = "2026-03-01 23:36:31 GMT";
-  public static final String GIT_BRANCH = "climbAlignPID";
-  public static final String BUILD_DATE = "2026-03-02 05:39:44 GMT";
-  public static final long BUILD_UNIX_TIME = 1772429984823L;
+  public static final int GIT_REVISION = 178;
+  public static final String GIT_SHA = "2861365bcbf988956228b824aeb8fcee6b8f47a2";
+  public static final String GIT_DATE = "2026-03-02 00:10:13 GMT";
+  public static final String GIT_BRANCH = "Shooter-Testing";
+  public static final String BUILD_DATE = "2026-03-02 00:18:26 GMT";
+  public static final long BUILD_UNIX_TIME = 1772410706920L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
