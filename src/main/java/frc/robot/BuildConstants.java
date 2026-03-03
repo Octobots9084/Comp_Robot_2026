c5e3d062f8cc8eeca0aa8a9c98c355bd8d23d960
SebastianBlackwell <sebastian.j.blackwell@gmail.com>
2026-03-02 20:17:58 -0800

    fixed it again

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,15 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 206;
-  public static final String GIT_SHA = "e22bc49b9a26a09f78ee6dbab807fcc200c9562c";
-  public static final String GIT_DATE = "2026-03-03 03:42:43 GMT";
-  public static final int GIT_REVISION = 206;
-  public static final String GIT_SHA = "e22bc49b9a26a09f78ee6dbab807fcc200c9562c";
-  public static final String GIT_DATE = "2026-03-03 03:42:43 GMT";
+  public static final int GIT_REVISION = 210;
+  public static final String GIT_SHA = "325c73027dee527edd437888e03571846dd07d1d";
+  public static final String GIT_DATE = "2026-03-03 04:16:59 GMT";
   public static final String GIT_BRANCH = "Shooter-Testing";
-  public static final String BUILD_DATE = "2026-03-03 03:57:05 GMT";
-  public static final long BUILD_UNIX_TIME = 1772510225669L;
+  public static final String BUILD_DATE = "2026-03-03 04:17:32 GMT";
+  public static final long BUILD_UNIX_TIME = 1772511452821L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
