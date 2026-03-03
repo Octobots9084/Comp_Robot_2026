cc798ca84e4cdf763e69aee97ac3c2e0d7332e5f
SebastianBlackwell <sebastian.j.blackwell@gmail.com>
2026-03-02 20:51:47 -0800

    fixed auto again

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 210;
-  public static final String GIT_SHA = "325c73027dee527edd437888e03571846dd07d1d";
-  public static final String GIT_DATE = "2026-03-03 04:16:59 GMT";
+  public static final int GIT_REVISION = 213;
+  public static final String GIT_SHA = "936e767f0b8787fb4143c89b364960e654a0e499";
+  public static final String GIT_DATE = "2026-03-03 04:43:25 GMT";
   public static final String GIT_BRANCH = "Shooter-Testing";
-  public static final String BUILD_DATE = "2026-03-03 04:17:32 GMT";
-  public static final long BUILD_UNIX_TIME = 1772511452821L;
+  public static final String BUILD_DATE = "2026-03-03 04:51:22 GMT";
+  public static final long BUILD_UNIX_TIME = 1772513482915L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
