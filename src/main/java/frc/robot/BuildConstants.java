f407ed1fb7985830a1f95b4e668176286409da45
SebastianBlackwell <sebastian.j.blackwell@gmail.com>
2026-03-03 19:49:09 -0800

    added ferry auto

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 226;
-  public static final String GIT_SHA = "d2fb35c5f0a150d0e84c788de0c7054e15ea9394";
-  public static final String GIT_DATE = "2026-03-04 03:17:30 GMT";
+  public static final int GIT_REVISION = 229;
+  public static final String GIT_SHA = "8210c760a688ed36afb00d2a53f00fac66df703c";
+  public static final String GIT_DATE = "2026-03-04 03:46:37 GMT";
   public static final String GIT_BRANCH = "Shooter-Testing";
-  public static final String BUILD_DATE = "2026-03-04 03:29:54 GMT";
-  public static final long BUILD_UNIX_TIME = 1772594994379L;
+  public static final String BUILD_DATE = "2026-03-04 03:48:28 GMT";
+  public static final long BUILD_UNIX_TIME = 1772596108218L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
