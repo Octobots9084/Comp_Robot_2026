233b5c9cd85681a8ec5126162b61376116a01213
SebastianBlackwell <sebastian.j.blackwell@gmail.com>
2026-03-03 20:34:58 -0800

    improved double mid wipe

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 229;
-  public static final String GIT_SHA = "8210c760a688ed36afb00d2a53f00fac66df703c";
-  public static final String GIT_DATE = "2026-03-04 03:46:37 GMT";
+  public static final int GIT_REVISION = 230;
+  public static final String GIT_SHA = "f407ed1fb7985830a1f95b4e668176286409da45";
+  public static final String GIT_DATE = "2026-03-04 03:49:09 GMT";
   public static final String GIT_BRANCH = "Shooter-Testing";
-  public static final String BUILD_DATE = "2026-03-04 03:48:28 GMT";
-  public static final long BUILD_UNIX_TIME = 1772596108218L;
+  public static final String BUILD_DATE = "2026-03-04 04:34:10 GMT";
+  public static final long BUILD_UNIX_TIME = 1772598850728L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
