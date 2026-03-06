8bb3c955123da3840b42dc911b8937acdf47fd43
MichaelStevenShinn <michaelstevenshinn@gmail.com>
2026-03-05 21:55:48 -0800

    last autos before hinime

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 245;
-  public static final String GIT_SHA = "91bb463d56c5d1230432bf6ac4553bc8215e9003";
-  public static final String GIT_DATE = "2026-03-05 05:43:46 GMT";
+  public static final int GIT_REVISION = 249;
+  public static final String GIT_SHA = "a4de08d61860117606fd74583ad2f2ecf6d2bcfe";
+  public static final String GIT_DATE = "2026-03-06 04:59:42 GMT";
   public static final String GIT_BRANCH = "Shooter-Testing";
-  public static final String BUILD_DATE = "2026-03-06 02:47:50 GMT";
-  public static final long BUILD_UNIX_TIME = 1772765270055L;
+  public static final String BUILD_DATE = "2026-03-06 05:36:58 GMT";
+  public static final long BUILD_UNIX_TIME = 1772775418478L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
