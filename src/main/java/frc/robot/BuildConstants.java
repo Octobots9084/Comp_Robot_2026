5c491bd17fd657902e2d700c1ad8bea919f3c688
Santiago7611 <bahlam935@gmail.com>
2026-03-07 08:32:56 -0800

    Simple Depot

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 249;
-  public static final String GIT_SHA = "a4de08d61860117606fd74583ad2f2ecf6d2bcfe";
-  public static final String GIT_DATE = "2026-03-06 04:59:42 GMT";
+  public static final int GIT_REVISION = 254;
+  public static final String GIT_SHA = "58e5893d15d5353e2b81d0f6615796d49cfedc80";
+  public static final String GIT_DATE = "2026-03-07 16:27:04 GMT";
   public static final String GIT_BRANCH = "Shooter-Testing";
-  public static final String BUILD_DATE = "2026-03-06 05:36:58 GMT";
-  public static final long BUILD_UNIX_TIME = 1772775418478L;
+  public static final String BUILD_DATE = "2026-03-07 16:31:36 GMT";
+  public static final long BUILD_UNIX_TIME = 1772901096157L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
