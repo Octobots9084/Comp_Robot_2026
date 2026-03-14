a130c5a3411bc00e0eba4146bc23e0ef6490d443
OliverHakansson <theoliverhakansson@gmail.com>
2026-03-13 18:15:13 -0700

    Rush to center + outpost is perfect

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 293;
-  public static final String GIT_SHA = "d6569faf404bf14d03b46d9d79d3ae615227c4c8";
-  public static final String GIT_DATE = "2026-03-13 03:44:45 GMT";
+  public static final int GIT_REVISION = 294;
+  public static final String GIT_SHA = "654a29ba476d96021cf328693359658138ca63b9";
+  public static final String GIT_DATE = "2026-03-13 05:57:43 GMT";
   public static final String GIT_BRANCH = "glendale-comp";
-  public static final String BUILD_DATE = "2026-03-13 05:54:40 GMT";
-  public static final long BUILD_UNIX_TIME = 1773381280969L;
+  public static final String BUILD_DATE = "2026-03-14 01:12:05 GMT";
+  public static final long BUILD_UNIX_TIME = 1773450725513L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
