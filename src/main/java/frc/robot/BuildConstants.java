f843b00d070ee4953c02360b92e7868752f52cf8
OliverHakansson <theoliverhakansson@gmail.com>
2026-03-13 23:44:05 -0700

    rush center depot done

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 298;
-  public static final String GIT_SHA = "5eb8be7d959583fcd421276346c4007fbd2e0264";
-  public static final String GIT_DATE = "2026-03-14 03:38:37 GMT";
+  public static final int GIT_REVISION = 300;
+  public static final String GIT_SHA = "36087bc76d0b19d4f4ef6f65ec65a22cacf586ad";
+  public static final String GIT_DATE = "2026-03-14 04:10:06 GMT";
   public static final String GIT_BRANCH = "glendale-comp";
-  public static final String BUILD_DATE = "2026-03-14 03:53:15 GMT";
-  public static final long BUILD_UNIX_TIME = 1773460395378L;
+  public static final String BUILD_DATE = "2026-03-14 06:40:41 GMT";
+  public static final long BUILD_UNIX_TIME = 1773470441203L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
