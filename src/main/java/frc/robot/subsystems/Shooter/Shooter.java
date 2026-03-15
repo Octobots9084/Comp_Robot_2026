8142e19c224d2957ee47d61056b0b13d850c1ba9
Oliver Hakansson <theoliverhakansson@gmail.com>
2026-03-15 13:09:08 -0700

    basic shooter interpolation

--- src/main/java/frc/robot/subsystems/Shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter/Shooter.java
@@ -176,23 +176,13 @@ public class Shooter extends SubsystemBase {
                 }
                 break;
             case HUB:
-                // hubBallSpeed = 
-                // SmartDashboard.getNumber("hubBallSpeed", 8);
-                // hubBallSpeed = 6.7
-                //7.05 +(8.13-6.7)*(getDistanceToHub()/4.18532579377);
-                hubBallSpeed = 0.5*(getDistanceToHub())+6;//8;//12;//6.5361+ 0.96897 * (getDistanceToHub()); // first change
-                hubFlywheelSpeed = 1.333333*getDistanceToHub()+5.133333;//7;//8.5;//6.3677 + 0.56653 * (getDistanceToHub()); // second change
+                // hubBallSpeed = 0.5*(getDistanceToHub())+6;//8;//12;//6.5361+ 0.96897 * (getDistanceToHub()); // first change
+                hubFlywheelSpeed = 0.458687*(getDistanceToHub() + 10.19436);//7;//8.5;//6.3677 + 0.56653 * (getDistanceToHub()); // second change
+                hubBallSpeed = 9;
                 Logger.recordOutput("hubBallSpeed",hubBallSpeed);
                 Logger.recordOutput("hubFlywheelSpeed",hubFlywheelSpeed);
                 isAimedAtHub = isAimedAtHub(hubBallSpeed);
-                // hubFlywheelSpeed = (hubBallSpeed-0.0482494)/0.673537;
-                // hubFlywheelSpeed = SmartDashboard.getNumber("hubFlywheelSpeed", 10.5);
-                // hubFlywheelSpeed = 9.5;
-
-
-                // isAimedAtHub = true;
-                // turret.setTurretPosition(-90.0/360.0);
-                // turret.setHoodPosition(75/360.0);
+                turret.setHoodPosition(-4.45537*(getDistanceToHub())+89.48911);
                 
                 if(swerve.isInAllianceZone() && (isHubActive())){
                     if(driverOverride){
