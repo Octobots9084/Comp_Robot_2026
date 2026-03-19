e6882626f551404a9a51f1959c0cc89cb2863baf
Jason Cox <jasondpc12@gmail.com>
2026-03-18 18:31:24 -0700

    fixed incomplete merge in BuildConstants.java

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,21 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-<<<<<<< Updated upstream
-  public static final int GIT_REVISION = 315;
-  public static final String GIT_SHA = "3c365d862601b428076fc9d9202e4b24bdc47729";
-  public static final String GIT_DATE = "2026-03-17 02:04:33 GMT";
-  public static final String GIT_BRANCH = "glendale-comp";
-  public static final String BUILD_DATE = "2026-03-17 02:39:38 GMT";
-  public static final long BUILD_UNIX_TIME = 1773715178840L;
-=======
   public static final int GIT_REVISION = 314;
   public static final String GIT_SHA = "f8dcd4f6405456af8484722d1e21f164474f5ec1";
   public static final String GIT_DATE = "2026-03-17 01:35:18 GMT";
   public static final String GIT_BRANCH = "glendale-comp";
   public static final String BUILD_DATE = "2026-03-17 02:14:46 GMT";
   public static final long BUILD_UNIX_TIME = 1773713686034L;
->>>>>>> Stashed changes
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
