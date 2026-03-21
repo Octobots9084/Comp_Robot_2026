cbc3c477f57b7d7dd9023a8666c3139a66a1286a
Oliver Hakansson <theoliverhakansson@gmail.com>
2026-03-20 18:53:27 -0700

    removing subsystem wrap from comp

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 327;
-  public static final String GIT_SHA = "1b37d0d497fc9dd7cd7a927974a9bf51afe755db";
-  public static final String GIT_DATE = "2026-03-19 03:10:24 GMT";
+  public static final int GIT_REVISION = 334;
+  public static final String GIT_SHA = "bf091016236ad0ad8df0a277ecd6c37f3c855f3a";
+  public static final String GIT_DATE = "2026-03-21 01:52:22 GMT";
   public static final String GIT_BRANCH = "glendale-comp";
-  public static final String BUILD_DATE = "2026-03-19 03:55:50 GMT";
-  public static final long BUILD_UNIX_TIME = 1773892550369L;
+  public static final String BUILD_DATE = "2026-03-21 01:53:03 GMT";
+  public static final long BUILD_UNIX_TIME = 1774057983880L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
