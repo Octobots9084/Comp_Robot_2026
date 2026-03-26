0e073a3cc704dbf242558cd308797f8c75a0288d
OliverHakansson <theoliverhakansson@gmail.com>
2026-03-25 17:11:52 -0700

    fixing lights again?

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 356;
-  public static final String GIT_SHA = "4ca29c692c285d0520c0a28661d2590e3922f4a1";
-  public static final String GIT_DATE = "2026-03-25 03:54:49 GMT";
+  public static final int GIT_REVISION = 362;
+  public static final String GIT_SHA = "17cf6720dc4199e63e009af22537c6b3c58bd19d";
+  public static final String GIT_DATE = "2026-03-26 00:02:57 GMT";
   public static final String GIT_BRANCH = "glendale-comp";
-  public static final String BUILD_DATE = "2026-03-25 05:29:39 GMT";
-  public static final long BUILD_UNIX_TIME = 1774416579941L;
+  public static final String BUILD_DATE = "2026-03-26 00:08:26 GMT";
+  public static final long BUILD_UNIX_TIME = 1774483706587L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
