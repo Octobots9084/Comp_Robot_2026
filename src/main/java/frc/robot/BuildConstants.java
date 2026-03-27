5e6d1813ccf1dbb7bfc9c3f0aa2ac9376f8031dc
OliverHakansson <theoliverhakansson@gmail.com>
2026-03-27 08:18:14 -0700

    michaels wack auto changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 379;
-  public static final String GIT_SHA = "4b958c38ffe27d03137490c881fd43e36da5d503";
-  public static final String GIT_DATE = "2026-03-27 06:52:02 GMT";
+  public static final int GIT_REVISION = 380;
+  public static final String GIT_SHA = "dfe371c2b74affe4de288770cb18bb3c6f5c565e";
+  public static final String GIT_DATE = "2026-03-27 15:00:55 GMT";
   public static final String GIT_BRANCH = "glendale-comp";
-  public static final String BUILD_DATE = "2026-03-27 07:25:48 GMT";
-  public static final long BUILD_UNIX_TIME = 1774596348763L;
+  public static final String BUILD_DATE = "2026-03-27 15:08:40 GMT";
+  public static final long BUILD_UNIX_TIME = 1774624120801L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
