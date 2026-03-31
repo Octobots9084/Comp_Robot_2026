ded1634531ab2c45051cf47c665a371fdcec3053
OliverHakansson <theoliverhakansson@gmail.com>
2026-03-30 17:20:36 -0700

    final glenadle changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 388;
-  public static final String GIT_SHA = "0b47ce20a048125c82453b094dbdb83cfe8e6cb5";
-  public static final String GIT_DATE = "2026-03-29 15:06:14 GMT";
+  public static final int GIT_REVISION = 390;
+  public static final String GIT_SHA = "dc186ac2574f4e6a7912cb8e1013f616bc754c1f";
+  public static final String GIT_DATE = "2026-03-29 17:46:24 GMT";
   public static final String GIT_BRANCH = "Glendale-sub-michael-and-santi-changes";
-  public static final String BUILD_DATE = "2026-03-29 16:42:24 GMT";
-  public static final long BUILD_UNIX_TIME = 1774802544559L;
+  public static final String BUILD_DATE = "2026-03-29 22:45:34 GMT";
+  public static final long BUILD_UNIX_TIME = 1774824334516L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
