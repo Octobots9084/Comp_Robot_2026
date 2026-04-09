a4ed34c809b4c6625a5214cb334ac0bc71af3296
OliverHakansson <theoliverhakansson@gmail.com>
2026-04-09 12:13:44 -0700

    changed flywheel kv to hit setpoint

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 458;
-  public static final String GIT_SHA = "ff59aa83c94d41d0e5fdc992aea801d0f7c9c849";
-  public static final String GIT_DATE = "2026-04-09 16:16:13 GMT";
+  public static final int GIT_REVISION = 459;
+  public static final String GIT_SHA = "ce9bff64e06ddcec1e4a1cfff9ce495840af6685";
+  public static final String GIT_DATE = "2026-04-09 19:02:34 GMT";
   public static final String GIT_BRANCH = "Anaheim";
-  public static final String BUILD_DATE = "2026-04-09 19:01:20 GMT";
-  public static final long BUILD_UNIX_TIME = 1775761280597L;
+  public static final String BUILD_DATE = "2026-04-09 19:10:47 GMT";
+  public static final long BUILD_UNIX_TIME = 1775761847654L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
