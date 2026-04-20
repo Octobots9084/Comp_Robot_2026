0461ace5fcc10bd8bb9df6e657711e25c02eec2e
MichaelStevenShinn <michaelstevenshinn@gmail.com>
2026-04-19 21:20:51 -0700

    way way way more autos

--- src/main/java/frc/robot/util/CreateAutoDuplicates.java
+++ src/main/java/frc/robot/util/CreateAutoDuplicates.java
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class CreateAutoDuplicates {
-    private static String oldAutoName = "Left";//put the path name here to duplicate, it will go in Generated Flipped Paths
+    private static String oldAutoName = "Center Left Hybrid";//put the path name here to duplicate, it will go in Generated Flipped Paths
     private static String oldAuto;
     private static String newAutoName;
     private static String newAuto;
