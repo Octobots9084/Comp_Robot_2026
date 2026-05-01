c34e5d93abe041904a1cc46327049edb9e70ea1e
MichaelStevenShinn <michaelstevenshinn@gmail.com>
2026-05-01 14:44:16 -0700

    auto bump faster

--- src/main/java/frc/robot/util/CreatePathDuplicates.java
+++ src/main/java/frc/robot/util/CreatePathDuplicates.java
@@ -18,11 +18,11 @@ import com.google.gson.GsonBuilder;
 
 public class CreatePathDuplicates {
     private static double m = 4;//meters, length of half way through the field (field length/2)
-    private static String oldPathName = "Left Sweep 1";//put the path name here to duplicate, it will go in Generated Flipped Paths
+    private static String oldPathName = "Right Sweep Hybrid 1";//put the path name here to duplicate, it will go in Generated Flipped Paths
     private static String oldPath;
     private static String newPathName;
     private static String newPath;
-    private static String goalPathFolder;
+    private static String goalPathFolder = "";
         
     public static void main(String[] args) {
         try {
@@ -34,7 +34,12 @@ public class CreatePathDuplicates {
             newPath = "src/main/deploy/pathplanner/paths/" + newPathName + ".path";
             Files.deleteIfExists(Path.of(newPath));
             // File newFile = new File(newPath);
-            goalPathFolder = args[1];
+            
+            
+            if (args.length > 1) {
+              goalPathFolder = args[1];
+            }
+            
             // newFile.createNewFile();
 
             //RN DOESNT DO MUCH TO THE PATH, NEEDS MORE TESTING
